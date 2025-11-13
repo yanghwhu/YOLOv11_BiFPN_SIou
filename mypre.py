01be7583af2e61d7eab56b27aa49748f28f3bd9b
yanghwhu <114213366+yanghwhu@users.noreply.github.com>
2025-11-13 20:21:28 +0800

    Update mypre.py

--- mypre.py
+++ mypre.py
@@ -4,6 +4,6 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('runs/train/exp/weights/best.pt')
+    model = YOLO('run/train/exp/weights/best.pt')
     results=model.predict(source='img/img1.png', imgsz=640, device='0', save=True, save_txt=True)
 
