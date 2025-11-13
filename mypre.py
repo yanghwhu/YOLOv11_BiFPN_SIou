94c4c27424ccf5d9760532e9f2767d34b6274ac3
yanghwhu <114213366+yanghwhu@users.noreply.github.com>
2025-11-13 15:43:50 +0800

    Add files via upload

---
+++ mypre.py
@@ -0,0 +1,9 @@
+import warnings
+
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('runs/train/exp/weights/best.pt')
+    results=model.predict(source='img/img1.png', imgsz=640, device='0', save=True, save_txt=True)
+
